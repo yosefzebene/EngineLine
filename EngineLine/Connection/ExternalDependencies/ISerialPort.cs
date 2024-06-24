22eb3546443b5cc244a75f050d4c2f9d0cf62e8c
yosef-ZzZ <yosef.zebene1@gmail.com>
2024-06-24 18:52:26 -0500

    Wrapper for SerialPort for testability

---
+++ EngineLine/Connection/ExternalDependencies/ISerialPort.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+namespace EngineLine.Connection.ExternalDependencies
+{
+    public interface ISerialPort
+    {
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public Parity Parity { get; set; }
+        public int DataBits { get; set; }
+        public StopBits StopBits { get; set; }
+        public Handshake Handshake { get; set; }
+
+        public void Open();
+        public void Close();
+        public void WriteLine(string message);
+        public string ReadExisting();
+    }
+}
---
+++ EngineLine/Connection/ExternalDependencies/SerialPortWrapper.cs
@@ -0,0 +1,55 @@
+﻿using System.IO.Ports;
+
+namespace EngineLine.Connection.ExternalDependencies
+{
+    public class SerialPortWrapper : ISerialPort
+    {
+        private readonly SerialPort _serialPort;
+
+        public string PortName 
+        {
+            get { return _serialPort.PortName; }
+            set { _serialPort.PortName = value; }
+        }
+
+        public int BaudRate
+        {
+            get { return _serialPort.BaudRate; }
+            set { _serialPort.BaudRate = value; }
+        }
+
+        public Parity Parity
+        {
+            get { return _serialPort.Parity; }
+            set { _serialPort.Parity = value; }
+        }
+
+        public int DataBits
+        {
+            get { return _serialPort.DataBits; }
+            set { _serialPort.DataBits = value; }
+        }
+        
+        public StopBits StopBits
+        {
+            get { return _serialPort.StopBits; }
+            set { _serialPort.StopBits = value; }
+        }
+
+        public Handshake Handshake
+        {
+            get { return _serialPort.Handshake; }
+            set { _serialPort.Handshake = value; }
+        }
+
+        public SerialPortWrapper() { _serialPort = new(); }
+
+        public void Open() { _serialPort.Open(); }
+
+        public void Close() { _serialPort.Close(); }
+
+        public void WriteLine(string message) { _serialPort.WriteLine(message); }
+
+        public string ReadExisting() { return _serialPort.ReadExisting(); }
+    }
+}
