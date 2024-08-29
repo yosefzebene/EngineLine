f04ea0309c0e6556e29fbbfaad8c0ebff135754f
yosef-ZzZ <yosef.zebene1@gmail.com>
2024-08-29 10:08:12 -0500

    Added very basic plotting page

---
+++ EngineLine/Pages/Plot.xaml.cs
@@ -0,0 +1,206 @@
+﻿using EngineLineLibrary.Vehicle.Enums;
+using Microsoft.UI.Xaml.Controls;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace EngineLine.Pages
+{
+    public sealed partial class Plot : Page
+    {
+        private Pid[] selectedPids;
+        private bool isMonitoring = false;
+        private const int MaxSecondsToShow = 20;
+
+        public Plot()
+        {
+            this.InitializeComponent();
+
+            PopulateSupportedPidsListView();
+            SetupPlotViewPlaceHolderModel();
+        }
+
+        private void PopulateSupportedPidsListView()
+        {
+            Dictionary<Pid, string> supportedPidsWithNames = new();
+            var supportedPids = ConnectionAccess.QueryManager.GetSupportedCommands();
+
+            foreach (var pid in supportedPids)
+            {
+                supportedPidsWithNames.Add(pid, pid.GetName());
+            }
+            SupportedPidsListView.ItemsSource = supportedPidsWithNames;
+            SupportedPidsListView.DisplayMemberPath = "Value";
+        }
+
+        private void SetupPlotViewPlaceHolderModel()
+        {
+            PlotModel placeHolderModel = new()
+            {
+                PlotAreaBorderColor = OxyColors.Transparent,
+                Axes =
+                {
+                    new DateTimeAxis {
+                        Title = "Time",
+                        Position = AxisPosition.Bottom,
+                        StringFormat = "hh:mm:ss",
+                        Angle = -45,
+                        Minimum = DateTimeAxis.ToDouble(DateTime.Now),
+                        Maximum = DateTimeAxis.ToDouble(DateTime.Now.AddSeconds(MaxSecondsToShow)),
+                        IsPanEnabled = true,
+                        IsZoomEnabled = false,
+                        IntervalType = DateTimeIntervalType.Seconds,
+                        MajorGridlineStyle = LineStyle.Dot,
+                        MajorGridlineColor = OxyColor.Parse("#4d4d4d"),
+                        AxislineColor = OxyColors.White,
+                        TicklineColor = OxyColors.White,
+                    }
+                }
+            };
+
+            plotView.Model = placeHolderModel;
+        }
+
+        private void SupportedPidsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedPids = SupportedPidsListView.SelectedItems.Cast<KeyValuePair<Pid, string>>().Select(kv => kv.Key).ToArray();
+        }
+
+        private void StartStopButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
+            isMonitoring = !isMonitoring;
+
+            startStopButton.Content = isMonitoring ? "STOP" : "START";
+
+            if (isMonitoring)
+            {
+                StartMonitoring();
+            }
+        }
+
+        private async void StartMonitoring()
+        {
+            PlotModel plotModel = CreateAndSetupModel();
+
+            plotView.Model = plotModel;
+
+            var dateTimeAxis = plotModel.Axes.OfType<DateTimeAxis>().First();
+
+            while (isMonitoring)
+            {
+                foreach (Pid pid in selectedPids)
+                {
+                    try
+                    {
+                        var task = Task.Factory.StartNew(() => ConnectionAccess.QueryManager.GetCurrentData(pid));
+                        var result = await task;
+
+                        var dateTimeNow = DateTime.Now;
+
+                        var pidLineSeries = (LineSeries)plotModel.Series.Where(s => s.Title == pid.GetName()).First();
+                        pidLineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(dateTimeNow), Decimal.ToDouble(result.Result)));
+
+                        CheckIfTimeAxesHasRunOutOfRoomAndPanIt(dateTimeAxis, dateTimeNow);
+
+                        plotModel.InvalidatePlot(true);
+                    }
+                    catch
+                    {
+                        // just skip it
+                    }
+                }
+            }
+        }
+
+        private PlotModel CreateAndSetupModel()
+        {
+            PlotModel plotModel = new()
+            {
+                PlotAreaBorderColor = OxyColors.Transparent,
+                Axes =
+                {
+                    new DateTimeAxis {
+                        Title = "Time",
+                        Position = AxisPosition.Bottom,
+                        StringFormat = "hh:mm:ss",
+                        Angle = -45,
+                        Minimum = DateTimeAxis.ToDouble(DateTime.Now),
+                        Maximum = DateTimeAxis.ToDouble(DateTime.Now.AddSeconds(MaxSecondsToShow)),
+                        IsPanEnabled = true,
+                        IsZoomEnabled = false,
+                        IntervalType = DateTimeIntervalType.Seconds,
+                        MajorGridlineStyle = LineStyle.Dot,
+                        MajorGridlineColor = OxyColor.Parse("#4d4d4d"),
+                        AxislineColor = OxyColors.White,
+                        TicklineColor = OxyColors.White,
+                    }
+                }
+            };
+
+            AddAxisAndLineSeriesOnTheModel(plotModel);
+
+            return plotModel;
+        }
+
+        private void AddAxisAndLineSeriesOnTheModel(PlotModel model)
+        {
+            Random random = new();
+
+            int positionTierCounter = 0;
+            foreach (Pid pid in selectedPids)
+            {
+                var name = pid.GetName();
+
+                model.Axes.Add(new LinearAxis()
+                {
+                    Title = name,
+                    Unit = pid.GetUnit(),
+                    Position = AxisPosition.Right,
+                    PositionTier = positionTierCounter,
+                    Key = name,
+                    Minimum = pid.GetMin(),
+                    Maximum = pid.GetMax(),
+                    IsZoomEnabled = false,
+                    IsPanEnabled = false,
+                    AxislineColor = OxyColors.White,
+                    TicklineColor = OxyColors.White
+                });
+
+                byte r = Convert.ToByte(random.Next(0, 256));
+                byte g = Convert.ToByte(random.Next(0, 256));
+                byte b = Convert.ToByte(random.Next(0, 256));
+
+                model.Series.Add(new LineSeries()
+                {
+                    Title = name,
+                    StrokeThickness = 2,
+                    Color = OxyColor.FromRgb(r, g, b),
+                    LineStyle = LineStyle.Solid,
+                    YAxisKey = name
+                });
+
+                positionTierCounter++;
+            }
+        }
+
+        private static void CheckIfTimeAxesHasRunOutOfRoomAndPanIt(DateTimeAxis dateTimeAxis, DateTime dateTimeNow)
+        {
+            if (DateTimeAxis.ToDateTime(dateTimeAxis.Maximum) < dateTimeNow)
+            {
+                dateTimeAxis.Minimum = DateTimeAxis.ToDouble(dateTimeNow.AddSeconds(-1 * MaxSecondsToShow));
+                dateTimeAxis.Maximum = DateTimeAxis.ToDouble(dateTimeNow);
+                dateTimeAxis.Reset();
+            }
+        }
+
+        private void Page_Unloaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            isMonitoring = false;
+        }
+    }
+}
--- EngineLineLibrary/Vehicle/Enums/Pid.cs
+++ EngineLineLibrary/Vehicle/Enums/Pid.cs
@@ -6,131 +6,249 @@ namespace EngineLineLibrary.Vehicle.Enums
     {
         [NameAndUnit("Supported PIDs 1 To 20", "")]
         SupportedPids1to20,
+
         [NameAndUnit("Monitor Status Since DTC Cleared", "")]
         MonitorStatusSinceDtcCleared,
+
         [NameAndUnit("DTC That Caused Freeze Frame", "")]
         DtcThatCausedFreezeFrame,
+
         [NameAndUnit("Fuel System Status", "")]
         FuelSystemStatus,
+
         [NameAndUnit("Calculated Engine Load", "%")]
+        [MinAndMax(0, 100)]
         CalculatedEngineLoad,
+
         [NameAndUnit("Engine Coolant Temperature", "°C")]
+        [MinAndMax(-40, 215)]
         EngineCoolantTemperature,
+
         [NameAndUnit("Short Term Fuel Trim Bank 1", "%")]
+        [MinAndMax(-100, 100)]
         ShortTermFuelTrimBank1,
-        [NameAndUnit("Long Term Fuel Trim Bank 1", "%")] 
+
+        [NameAndUnit("Long Term Fuel Trim Bank 1", "%")]
+        [MinAndMax(-100, 100)]
         LongTermFuelTrimBank1,
+
         [NameAndUnit("Short Term Fuel Trim Bank 2", "%")]
+        [MinAndMax(-100, 100)]
         ShortTermFuelTrimBank2,
-        [NameAndUnit("Long Term Fuel Trim Bank 2", "%")] 
+
+        [NameAndUnit("Long Term Fuel Trim Bank 2", "%")]
+        [MinAndMax(-100, 100)]
         LongTermFuelTrimBank2,
-        [NameAndUnit("Fuel Pressure", "kPa")] 
+
+        [NameAndUnit("Fuel Pressure", "kPa")]
+        [MinAndMax(0, 765)]
         FuelPressure,
-        [NameAndUnit("Intake Manifold Absolute Pressure", "kPa")] 
+
+        [NameAndUnit("Intake Manifold Absolute Pressure", "kPa")]
+        [MinAndMax(0, 255)]
         IntakeManifoldAbsolutePressure,
+
         [NameAndUnit("Engine Speed", "rpm")]
+        [MinAndMax(0, 16_384)]
         EngineSpeed,
+
         [NameAndUnit("Vehicle Speed", "km/h")]
+        [MinAndMax(0, 255)]
         VehicleSpeed,
-        [NameAndUnit("Timing Advance", "°")] 
+
+        [NameAndUnit("Timing Advance", "°")]
+        [MinAndMax(-64, 64)]
         TimingAdvance,
-        [NameAndUnit("Intake Air Temperature", "°C")] 
+
+        [NameAndUnit("Intake Air Temperature", "°C")]
+        [MinAndMax(-40, 215)]
         IntakeAirTemperature,
-        [NameAndUnit("Mass Air Flow Sensor", "g/s")] 
+
+        [NameAndUnit("Mass Air Flow Sensor", "g/s")]
+        [MinAndMax(0, 656)]
         MassAirFlowSensor,
+
         [NameAndUnit("Throttle Position", "%")]
+        [MinAndMax(0, 100)]
         ThrottlePosition,
+
         [NameAndUnit("Commanded Secondary Air Status", "")] 
         CommandedSecondaryAirStatus,
+
         [NameAndUnit("Oxygen Sensors Present In 2 Banks", "")] 
         OxygenSensorsPresent2Banks,
-        [NameAndUnit("Oxygen Sensor 1 Voltage", "V")] 
+
+        [NameAndUnit("Oxygen Sensor 1 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor1Voltage,
+
         [NameAndUnit("Oxygen Sensor 2 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor2Voltage,
+
         [NameAndUnit("Oxygen Sensor 3 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor3Voltage,
+
         [NameAndUnit("Oxygen Sensor 4 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor4Voltage,
+
         [NameAndUnit("Oxygen Sensor 5 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor5Voltage,
+
         [NameAndUnit("Oxygen Sensor 6 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor6Voltage,
+
         [NameAndUnit("Oxygen Sensor 7 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor7Voltage,
+
         [NameAndUnit("Oxygen Sensor 8 Voltage", "V")]
+        [MinAndMax(0, 2)]
         OxygenSensor8Voltage,
+
         [NameAndUnit("Obd Standard", "")]
+        [MinAndMax(1, 250)]
         ObdStandard,
+
         [NameAndUnit("Oxygen Sensors Present In 4 Banks", "")]
         OxygenSensorsPresent4Banks,
+
         [NameAndUnit("Auxiliary Input Status", "")]
         AuxiliaryInputStatus,
+
         [NameAndUnit("Run Time Since Start", "s")]
+        [MinAndMax(0, 65_535)]
         RunTimeSinceStart,
+
         [NameAndUnit("Supported PIDs 21 To 40", "")]
         SupportedPids21to40,
-        [NameAndUnit("Distance Travel Since Check Engine Light On", "km")] 
+
+        [NameAndUnit("Distance Travel Since Check Engine Light On", "km")]
+        [MinAndMax(0, 65_535)]
         DistanceTravelSinceCheckEngineLightOn,
-        [NameAndUnit("Fuel Rail Pressure", "kPa")] 
+
+        [NameAndUnit("Fuel Rail Pressure", "kPa")]
+        [MinAndMax(0, 5178)]
         FuelRailPressure,
-        [NameAndUnit("Fuel Rail Gauge Pressure", "kPa")] 
+
+        [NameAndUnit("Fuel Rail Gauge Pressure", "kPa")]
+        [MinAndMax(0, 655_350)]
         FuelRailGaugePressure,
+
         [NameAndUnit("Wide Band Oxygen Sensor 1 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor1Voltage,
-        [NameAndUnit("Wide Band Oxygen Sensor 2 Voltage", "V")] 
+
+        [NameAndUnit("Wide Band Oxygen Sensor 2 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor2Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 3 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor3Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 4 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor4Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 5 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor5Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 6 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor6Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 7 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor7Voltage,
+
         [NameAndUnit("Wide Band Oxygen Sensor 8 Voltage", "V")]
+        [MinAndMax(0, 8)]
         WideBandOxygenSensor8Voltage,
+
         [NameAndUnit("Commanded Egr", "%")]
+        [MinAndMax(0, 100)]
         CommandedEgr,
+
         [NameAndUnit("Egr Error", "%")]
+        [MinAndMax(-100, 100)]
         EgrError,
+
         [NameAndUnit("Commanded Evap Purge", "%")]
+        [MinAndMax(0, 100)]
         CommandedEvapPurge,
+
         [NameAndUnit("Fuel Tank Level", "%")]
+        [MinAndMax(0, 100)]
         FuelTankLevel,
+
         [NameAndUnit("Warmups Since Codes Cleared", "")]
+        [MinAndMax(0, 255)]
         WarmupsSinceCodesCleared,
+
         [NameAndUnit("Distance Traveled Since Codes Cleared", "km")]
+        [MinAndMax(0, 65_535)]
         DistanceTraveledSinceCodesCleared,
+
         [NameAndUnit("Evap System Vapor Pressure", "Pa")]
+        [MinAndMax(-8192, 8192)]
         EvapSystemVaporPressure,
+
         [NameAndUnit("Absolute Barometric Pressure", "kPa")]
+        [MinAndMax(0, 255)]
         AbsoluteBarometricPressure,
+
         [NameAndUnit("Wide Band Oxygen Sensor 1 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor1Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 2 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor2Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 3 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor3Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 4 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor4Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 5 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor5Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 6 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor6Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 7 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor7Current,
+
         [NameAndUnit("Wide Band Oxygen Sensor 8 Current", "mA")]
+        [MinAndMax(-128, 128)]
         WideBandOxygenSensor8Current,
+
         [NameAndUnit("Catalyst Temperature Bank 1 Sensor 1", "°C")]
+        [MinAndMax(-128, 128)]
         CatalystTemperatureBank1Sensor1,
+
         [NameAndUnit("Catalyst Temperature Bank 2 Sensor 1", "°C")]
+        [MinAndMax(-128, 128)]
         CatalystTemperatureBank2Sensor1,
+
         [NameAndUnit("Catalyst Temperature Bank 1 Sensor 2", "°C")]
+        [MinAndMax(-128, 128)]
         CatalystTemperatureBank1Sensor2,
+
         [NameAndUnit("Catalyst Temperature Bank 2 Sensor 2", "°C")]
+        [MinAndMax(-128, 128)]
         CatalystTemperatureBank2Sensor2,
     }
 
@@ -144,6 +262,19 @@ namespace EngineLineLibrary.Vehicle.Enums
         {
             Name = name;
             Unit = unit;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class MinAndMaxAttribute : Attribute
+    {
+        public int Min { get; private set; }
+        public int Max { get; private set; }
+
+        public MinAndMaxAttribute(int min, int max)
+        {
+            Min = min;
+            Max = max;
         }
     }
 
@@ -175,6 +306,34 @@ namespace EngineLineLibrary.Vehicle.Enums
             }
 
             return "";
+        }
+
+        public static int GetMin(this Pid pid)
+        {
+            var type = pid.GetType();
+            var fieldInfo = type.GetField(pid.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attribute = fieldInfo.GetCustomAttribute<MinAndMaxAttribute>();
+                return attribute?.Min ?? 0;
+            }
+
+            return 0;
+        }
+
+        public static int GetMax(this Pid pid)
+        {
+            var type = pid.GetType();
+            var fieldInfo = type.GetField(pid.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attribute = fieldInfo.GetCustomAttribute<MinAndMaxAttribute>();
+                return attribute?.Max ?? 0;
+            }
+
+            return 0;
         }
     }
 }
